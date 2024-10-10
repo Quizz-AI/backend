4da582ea0f69797d2acc6f84d780d98db9b19b42
Gabriel Saldanha <gabrielsdnegre@gmail.com>
2024-10-10 15:47:42 -0300

    arch: initial spring inicialization

---
+++ src/test/java/com/quizzai/quizzai_api/QuizzaiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quizzai.quizzai_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizzaiApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
